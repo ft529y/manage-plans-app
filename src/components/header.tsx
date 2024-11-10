ce0736afc565069a034e98c4c27f8dfce28ebbae
Yuto Kawauchi <yuto529reds@gmail.com>
2024-11-10 16:11:36 +0900

    add logout function

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { signOut } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -12,23 +13,35 @@ const Header = () => {
       { label: 'Schedule', url: '/Calender' },
       { label: 'List', url: '/List-form' },
       { label: 'memo', url: '/memo' },
-      // { label: 'Logout', url: '/logout' },
     ],
   };
+
+  const logout = async () => {
+    await signOut({ callbackUrl: '/' });
+  };
+
   return (
     <>
       <header className="text-xl bg-slate-700 py-8 sticky top-0">
-        <nav className="flex  sm:justify-center space-x-4">
+        <nav className="flex  sm:justify-between items-center p-2">
           <Image src={iconPic} alt="Picture of the author" />
-          {links.linkPair.map((item) => (
-            <Link
-              className="font-mono text-violet-50 rounded-lg px-3 py-2 text-slate-700	font-medium hover:bg-slate-100 hover:text-purple-400"
-              key={item.label}
-              href={item.url}
-            >
-              {item.label}
-            </Link>
-          ))}
+          <div className=" flex space-x-4">
+            {links.linkPair.map((item) => (
+              <Link
+                className="font-mono text-violet-50 rounded-lg px-3 py-2 text-slate-700	font-medium hover:bg-slate-100 hover:text-purple-400"
+                key={item.label}
+                href={item.url}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </div>
+          <button
+            className="font-mono text-violet-50 rounded-full px-3 py-2 pr-16	font-medium hover:bg-red-600"
+            onClick={async () => logout()}
+          >
+            ログアウト
+          </button>
         </nav>
       </header>
     </>
