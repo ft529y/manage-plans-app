718cd1298f0606ad94551778cc1092bd28601d5e
Yuto Kawauchi <yuto529reds@gmail.com>
2025-01-06 17:24:47 +0900

    add modal function

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'scale-in-center':
+          'scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both',
+      },
+      keyframes: {
+        'scale-in-center': {
+          '0%': {
+            transform: 'scale(0)',
+            opacity: '1',
+          },
+          to: {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
