47e520031732a306966c80e8756cff660c452331
Yuto Kawauchi <yuto529reds@gmail.com>
2025-01-26 18:13:54 +0900

    add mongo schema

--- src/pages/utils/shemaModels.ts
+++ src/pages/utils/shemaModels.ts
@@ -1,10 +1,20 @@
 import mongoose from 'mongoose';
 
+// 以下にDBのスキーマ情報を記載していく。
 const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({});
+const CalenderSchema = new Schema({});
 const ListSchema = new Schema({
   id: String,
   listName: { formData: { text: String } },
 });
+const MemoSchema = new Schema({});
 
-export const ListModel =
-  mongoose.models.List || mongoose.model('List', ListSchema);
+const UserModel = mongoose.models.User || mongoose.model('User', UserSchema);
+const CalenderModel =
+  mongoose.models.Calender || mongoose.model('Calender', CalenderSchema);
+const ListModel = mongoose.models.List || mongoose.model('List', ListSchema);
+const MemoModel = mongoose.models.Memo || mongoose.model('Memo', MemoSchema);
+
+export { UserModel, CalenderModel, ListModel, MemoModel };
