4296712d5d32f49db2a6e70f12f24101f7d41f16
Yuto Kawauchi <yuto529reds@gmail.com>
2025-01-26 18:10:51 +0900

    add Mock data

---
+++ src/mock/listDataMock.ts
@@ -0,0 +1,16 @@
+import { ListData } from '@/common/types';
+
+export const listDataMock: ListData[] = [
+  {
+    id: '1',
+    listName: { formData: { text: '確認テスト' } },
+  },
+  {
+    id: '2',
+    listName: { formData: { text: '野菜' } },
+  },
+  {
+    id: '3',
+    listName: { formData: { text: '果物' } },
+  },
+];
