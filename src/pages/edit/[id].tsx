757f32c0057bd0f78ca69a7964b7e46b358d8198
Yuto Kawauchi <yuto529reds@gmail.com>
2025-04-06 19:05:35 +0900

    modify UI of edit page

--- src/pages/edit/[id].tsx
+++ src/pages/edit/[id].tsx
@@ -94,11 +94,20 @@ const EditPage = () => {
           placeholder="ここにメッセージを入力してください..."
           className="mt-2 w-full p-4 text-sm text-gray-700 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200 ease-in-out resize-none"
         ></textarea>
-      </div>
-      ---------------------------------------
-      <div>
-        <button onClick={() => updateItem()}>更新</button>
-        <button onClick={() => prev()}>戻る</button>
+        <div className="flex justify-end">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 mr-4 text-white py-2 px-4 rounded-full"
+            onClick={() => updateItem()}
+          >
+            登録
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 mr-4 text-white py-2 px-4 rounded-full"
+            onClick={() => prev()}
+          >
+            戻る
+          </button>
+        </div>
       </div>
     </>
   );
